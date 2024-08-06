c3881adb52d7e5759a541eecb8d3822c0e0e83d7
Over-Night <hidden98716@gmail.com>
2024-08-06 17:22:42 +0900

    fix login.vue error

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,11 +1,12 @@
 <template>
-  <div> c;ass="login"
+  <div class="login">
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
+    <a>Text</a>
   </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
